71fb4e46064ce90a8f33f9757f4b703aa09dca0a
Tomas Galvan-Huerta <tomas.huerta.94@gmail.com>
2024-12-15 17:51:15 -0800

    Initial Commit

---
+++ Weather App Take Home Test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Weather App Take Home Test
+//
+//  Created by Tomas Galvan-Huerta on 12/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Weather App Take Home Test/Weather_App_Take_Home_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_App_Take_Home_TestApp.swift
+//  Weather App Take Home Test
+//
+//  Created by Tomas Galvan-Huerta on 12/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Weather_App_Take_Home_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
